d00781f03525a26d7a3c4851ce5844ef3fc56c90
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2025-04-06 16:30:52 +0700

    create features component this component render list of site feature card create feature type and mock data

--- src/pages/customer/Home.tsx
+++ src/pages/customer/Home.tsx
@@ -1,14 +1,17 @@
 import { Banner } from "@/components/customer/home/Banner";
+import { Features } from "@/components/customer/home/Features";
 import { Introduction } from "@/components/customer/home/Introduction";
 import { NewCollection } from "@/components/customer/home/NewCollection";
+import { features } from "@/mock/features";
 import { introduction1, introduction2 } from "@/mock/introction";
 
 export const Home = () => {
   return (
-    <div className="">
+    <div className="flex flex-col gap-4">
       <Banner />
       <NewCollection />
       <Introduction {...introduction1} />
+      <Features features={features} />
       <Introduction reverse {...introduction2} />
     </div>
   );
