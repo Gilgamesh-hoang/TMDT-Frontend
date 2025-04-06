92185cbb95c6e208fe8cb4dc6b89b803baf7972c
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2025-04-06 15:53:53 +0700

    create introduction component this component render its content conditionally with reverse prop create introduction type and mock data

---
+++ src/mock/introction.ts
@@ -0,0 +1,26 @@
+import { IntroductionProps } from "@/components/customer/home/Introduction";
+
+export const introduction1: IntroductionProps = {
+  href: "/",
+  title: "Lorem ipsum dolor sit amet ",
+  description: `Dignissim lacus, turpis ut suspendisse vel tellus. Turpis purus,
+            gravida orci, fringilla a. Ac sed eu fringilla odio mi. Consequat
+            pharetra at magna imperdiet cursus ac faucibus sit libero. Ultricies
+            quam nunc, lorem sit lorem urna, pretium aliquam ut. In vel, quis
+            donec dolor id in. Pulvinar commodo mollis diam sed facilisis at
+            cursus imperdiet cursus ac faucibus sit faucibus sit libero.`,
+  imageUrl:
+    "https://file.hstatic.net/200000887785/file/qua_tang_83_y_nghia_14_142c74dd8d154b5cadfe9e7e59296115_grande.jpg",
+};
+export const introduction2: IntroductionProps = {
+  href: "/",
+  title: "Lorem ipsum dolor sit amet ",
+  description: `Dignissim lacus, turpis ut suspendisse vel tellus. Turpis purus,
+            gravida orci, fringilla a. Ac sed eu fringilla odio mi. Consequat
+            pharetra at magna imperdiet cursus ac faucibus sit libero. Ultricies
+            quam nunc, lorem sit lorem urna, pretium aliquam ut. In vel, quis
+            donec dolor id in. Pulvinar commodo mollis diam sed facilisis at
+            cursus imperdiet cursus ac faucibus sit faucibus sit libero.`,
+  imageUrl:
+    "https://caogam.vn/sites/default/files/styles/anh_detail/public/2021-12/tinh-dau-bac-ha.jpg?itok=Oc-j5mpL",
+};
--- src/pages/customer/Home.tsx
+++ src/pages/customer/Home.tsx
@@ -1,11 +1,15 @@
 import { Banner } from "@/components/customer/home/Banner";
+import { Introduction } from "@/components/customer/home/Introduction";
 import { NewCollection } from "@/components/customer/home/NewCollection";
+import { introduction1, introduction2 } from "@/mock/introction";
 
 export const Home = () => {
   return (
     <div className="">
       <Banner />
       <NewCollection />
+      <Introduction {...introduction1} />
+      <Introduction reverse {...introduction2} />
     </div>
   );
 };
