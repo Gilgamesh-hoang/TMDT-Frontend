c0bd645c47bbd3ba07d9f86d9f7584c76b781818
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2025-04-10 18:29:44 +0700

    create charts component for dashboard use react chart 2 lib

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,11 +1,17 @@
+import { OrderChart } from "@/components/admin/dashboard/OrderChart";
 import { RecentCustomers } from "@/components/admin/dashboard/RecentCustomers";
 import { RecentOrders } from "@/components/admin/dashboard/RecentOrders";
+import { RevenueChart } from "@/components/admin/dashboard/RevenueChart";
 import { WidgetSummaries } from "@/components/admin/dashboard/WidgetSummaries";
 
 export const Dashboard = () => {
   return (
-    <div className="p-4 w-full flex flex-col gap-y-6  h-[1000px]">
+    <div className="px-4 my-2 w-full flex flex-col gap-y-5 ">
       <WidgetSummaries />
+      <div className="grid grid-cols-2 gap-x-4">
+        <RevenueChart />
+        <OrderChart />
+      </div>
       <div className="grid grid-cols-2 gap-x-4">
         <RecentOrders />
         <RecentCustomers />
