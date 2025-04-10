f9fcca98faf708ef90db5188793d795cf61b4878
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2025-04-10 15:44:43 +0700

    render summary data table for recent orders and customers install data table from shadcn create datable component to render summary data without action button crete order type, order mock data render recent orders in the table similar thing for recent registered customers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
+import { OrderStatus } from "@/components/admin/dashboard/RecentOrders";
 import { clsx, type ClassValue } from "clsx";
-import { v4 as uuidv4 } from "uuid";
 import { twMerge } from "tailwind-merge";
-
+import { v4 as uuidv4 } from "uuid";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -13,4 +13,11 @@ export const formatCurrency = (amount: number) => {
 };
 export const uuid = () => {
   return uuidv4();
+};
+
+export const statusColorMap: Record<OrderStatus, string> = {
+  Success: "bg-success ",
+  Failed: "bg-error",
+  Pending: "bg-amber-500",
+  Processing: "bg-blue-500",
 };
--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,45 +1,14 @@
-import {
-  AnalyticsWidgetSummary,
-  AnalyticsWidgetSummaryProps,
-} from "@/components/admin/dashboard/AnalyticsWidgetSummary";
-import { uuid } from "@/lib/utils";
-import { Warehouse } from "lucide-react";
-import { FaTruck, FaUsers } from "react-icons/fa";
-import { FaSackDollar } from "react-icons/fa6";
+import { RecentCustomers } from "@/components/admin/dashboard/RecentCustomers";
+import { RecentOrders } from "@/components/admin/dashboard/RecentOrders";
+import { WidgetSummaries } from "@/components/admin/dashboard/WidgetSummaries";
 
 export const Dashboard = () => {
-  const analyticsSummaries: AnalyticsWidgetSummaryProps[] = [
-    {
-      icon: <FaSackDollar size={60} />,
-      title: "Doanh thu",
-      color: "green",
-      total: 123,
-    },
-    {
-      icon: <FaTruck size={60} />,
-      title: "Đơn hàng",
-      color: "orange",
-      total: 223,
-    },
-    {
-      icon: <FaUsers size={60} />,
-      title: "Khách hàng",
-      color: "fuchsia",
-      total: 223,
-    },
-    {
-      icon: <Warehouse size={60} />,
-      title: "Sản phẩm",
-      color: "blue",
-      total: 223,
-    },
-  ];
   return (
-    <div className="p-4 w-full ">
-      <div className="grid grid-cols-4 gap-x-6">
-        {analyticsSummaries.map((summary) => (
-          <AnalyticsWidgetSummary key={uuid()} {...summary} />
-        ))}
+    <div className="p-4 w-full flex flex-col gap-y-6  h-[1000px]">
+      <WidgetSummaries />
+      <div className="grid grid-cols-2 gap-x-4">
+        <RecentOrders />
+        <RecentCustomers />
       </div>
     </div>
   );
